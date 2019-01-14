6a1e6f8a3bc191741cb6b1f35f9c763511818b71
cuij1994 <894778463@qq.com>
2019-01-14 19:07:25 +0800

    第二次提交项目

---
+++ src/main/java/com/baizhi/cc/entity/User.java
@@ -0,0 +1,56 @@
+package com.baizhi.cc.entity;
+
+/**
+ * Created by Administrator on 2019/1/14 0014.
+ */
+public class User {
+
+    private String uid;
+    private String name;
+    private String password;
+
+
+    public User() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String uid, String name, String password) {
+
+        this.uid = uid;
+        this.name = name;
+        this.password = password;
+    }
+}
